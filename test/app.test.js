c4f108e141e3af43d07a0a2d5d95b3149328108c
janpetsa <janpetsa@hotmail.fi>
2020-12-15 17:56:19 +0200

    uusi versio

--- test/app.test.js
+++ test/app.test.js
@@ -4,14 +4,14 @@ var app = require('../app'),
 
 describe('GET /', function(){
   before(function() {
-      var port = '3000';
+      var port = '3001';
       app.set('port', port);
       var server = http.createServer(app);
       server.listen(port);
   });
 
   it('should return a 200 status code', function (done){
-    http.get({ host: '0.0.0.0', port: 3000 }, function(res) {
+    http.get({ host: 'localhost', port: 3001 }, function(res) {
       assert.deepEqual(res.statusCode, 200);
       done();
     }).on('error', function(e) {
